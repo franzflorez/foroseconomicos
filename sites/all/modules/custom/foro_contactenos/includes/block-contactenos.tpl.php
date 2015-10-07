b98b43a362081f144e732c085f9f4d26dad23b33
Franz Anderson Florez Ramirez <faflorez@caracoltv.com.co>
2015-10-07 08:12:10 -0500

    modulo custom de foros contactenos

--- sites/all/modules/custom/foro_contactenos/includes/block-contactenos.tpl.php
+++ sites/all/modules/custom/foro_contactenos/includes/block-contactenos.tpl.php
@@ -1,3 +1,6 @@
-<div class="form-class-contacto">
-    <?php echo $variables["contacto"]; ?>
+<div class="base-all">
+    <div class="logo">Logo</div>
+    <div class="form-class-contacto">
+        <?php echo $variables["contacto"]; ?>
+    </div>
 </div>
