df00d88c365792ebe96e326e61ea023dc4bb1e10
franzflorez <faflorez@caracoltv.com.co>
2015-10-07 12:34:53 -0500

    imagen de logo foros economicos

--- sites/all/modules/custom/foro_contactenos/includes/block-contactenos.tpl.php
+++ sites/all/modules/custom/foro_contactenos/includes/block-contactenos.tpl.php
@@ -1,14 +1,26 @@
 <div class="base-all">
     <div class="logo">
         <img width="340" title="foros económicos" src="http://www.foroseconomicos.com/sites/all/modules/custom/foro_contactenos/img/foros_economicos-2.jpg"/>
+        <div style="clear:both"></div>
     </div>
     <div class="mensaje">
         <h2>Contáctenos</h2>
         <p>
             Si desea más información del proximo evento, dejenos sus datos y pronto nos comunicaremos con usted para brindarle mayor información. 
         </p>
+        <div style="clear:both"></div>
     </div>
     <div class="form-class-contacto">
         <?php echo $variables["contacto"]; ?>
+        <div style="clear:both"></div>
     </div>
+    <div style="clear:both"></div>
 </div>
+<style>
+.base-all .mensaje{  
+    width: 780px;
+    margin: -102px 0 0 -90px;
+    text-align: center;    
+}
+</style>
+    
