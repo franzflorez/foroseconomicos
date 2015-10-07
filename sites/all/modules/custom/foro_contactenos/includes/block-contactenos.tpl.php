71ca60737c55f3e4fee3df20c3e314c65e69c610
Franz Anderson Florez Ramirez <faflorez@caracoltv.com.co>
2015-10-07 07:44:54 -0500

    modulo custom de foros contactenos

--- sites/all/modules/custom/foro_contactenos/includes/block-contactenos.tpl.php
+++ sites/all/modules/custom/foro_contactenos/includes/block-contactenos.tpl.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" version="XHTML+RDFa 1.0" dir="ltr">
-    <head>
-        <title>Foros Económicos Colombia | contactenos</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!--<link type="text/css" rel="stylesheet" media="all" href="" />
-        <script type="text/javascript" src=></script>-->
-    </head> 
-    <body>
-        <?php global $variables; ?>
-        <div id="messages"><?php print theme_status_messages($variables); ?></div>
-        <?php echo $variables["contacto"]; ?>
-    </body>
-</html>
+<div class="form-class-contacto">
+    <?php echo $variables["contacto"]; ?>
+</div>
