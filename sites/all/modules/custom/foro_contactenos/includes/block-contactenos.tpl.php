c65ea6ba15aa2ad3cc9e606824359fcb34ec2e1c
Franz Anderson Florez Ramirez <faflorez@caracoltv.com.co>
2015-10-07 07:11:22 -0500

    modulo custom de foros contactenos

---
+++ sites/all/modules/custom/foro_contactenos/includes/block-contactenos.tpl.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" version="XHTML+RDFa 1.0" dir="ltr">
+    <head>
+        <title>Foros Económicos Colombia | contactenos</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--<link type="text/css" rel="stylesheet" media="all" href="" />
+        <script type="text/javascript" src=></script>-->
+    </head> 
+    <body>
+        <h1>Contactenos</h1>
+        
+    </body>
+</html>
