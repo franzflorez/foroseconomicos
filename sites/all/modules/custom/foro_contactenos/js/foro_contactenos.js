8c13095aee3608e2353e5422c857b6c87379a83e
franzflorez <faflorez@caracoltv.com.co>
2015-10-16 13:59:49 -0500

    nuevo bloque de indadores en el modulo custom

--- sites/all/modules/custom/foro_contactenos/js/foro_contactenos.js
+++ sites/all/modules/custom/foro_contactenos/js/foro_contactenos.js
@@ -1,28 +1,26 @@
 (function($) {
     Drupal.behaviors.actualizarPageHome = {
         attach: function(context, settings) {
-            
-            //if($('.datos-bolsa-contenedor').length){
-                $.ajax({
-                        url: 'https://widgetsdataifx.blob.core.windows.net/dataifx/listcurrenciescorredores?a='+Math.random(),
+            if($('.datos-bolsa-contenedor').length){
+                jQuery.ajax({
+                        url: 'http://www.foroseconomicos.com/sites/all/modules/custom/foro_contactenos/js/jsondatabolsa.js?a='+Math.random(),
                         async: true,
-                        headers: {
-                            Origin:"http://www.foroseconomicos.com"
-                        },
+                        dataType: "json",
                         success: function(data) {
                             if(data == null){
                                 //alert('Imposible conectar con el servidor');
                                 return false;
                             }else{
-                                console.log("forosoooo");
-                                console.log(data);
+                                for (var bolsa in data){
+                                    //console.log(data[bolsa]['Id']);
+                                }
                             }
                         },
                         error: function(xhr, ajaxOptions, thrownError) {
-                            console.log("forosoooo 2");
+                            //console.log("forosoooo 2");
                         }
-                 });                
-            //}
+                 });               
+            }
             
         }
     };
