03006e1ac291dfe2c0e018d588e4ebda016b906a
franzflorez <faflorez@caracoltv.com.co>
2015-10-20 13:27:04 -0500

    pin Esto lo solicita Google para hacer seguimiento a las campañas google adds

--- sites/all/themes/multipurpose/templates/html.tpl.php
+++ sites/all/themes/multipurpose/templates/html.tpl.php
@@ -6,6 +6,19 @@
 <?php print $styles; ?>
 <?php print $scripts; ?>
 <!--[if lt IE 9]><script src="<?php print base_path() . drupal_get_path('theme', 'multipurpose') . '/js/html5.js'; ?>"></script><![endif]-->
+<script>
+!function(g,s,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','//;
+
+fbq('init', '44546536565907');
+fbq('track', "PageView");</script>
+<noscript><img height="1" width="1" style="display:none"
+src=/></noscript>
+<!-- End Pixel Code -->
+
 </head>
 <body class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php print $page_top; ?>
